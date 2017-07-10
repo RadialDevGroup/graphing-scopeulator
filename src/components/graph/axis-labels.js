bde000fab66b3df7bfaa5593991f4cbb03b98c71
Marshall Smith <marshall.ds@gmail.com>
2017-07-10 17:26:35 -0600

    symbols for labels

--- src/components/graph/axis-labels.js
+++ src/components/graph/axis-labels.js
@@ -1,16 +1,20 @@
 import _ from 'lodash';
 import React from 'react';
 
+const Label = ({x, y, size}) => (
+  <circle className="axis-label-symbol" cx={x} cy={y} r={size*(size*2/3)+1}/>
+);
+
 export default ({xScale, yScale, GRAPH_MARGIN, GRAPH_HEIGHT, GRAPH_WIDTH}) => {
   return (
     <g className="axis-labels">
       {_.times(4).map((i) => {
         const yAnchor = (i+1)*yScale + GRAPH_MARGIN;
-        return <text key={i} x={25} y={-yAnchor}>{i+1}</text>;
+        return <Label key={i} x={25} y={-yAnchor} size={i+1}/>;
       })}
       {_.times(4).map((i) => {
         const xAnchor = (i+1)*xScale + GRAPH_MARGIN;
-        return <text key={i} x={xAnchor} y={-25}>{i+1}</text>;
+        return <Label key={i} x={xAnchor} y={-25} size={i+1}/>;
       })}
     </g>
   );
